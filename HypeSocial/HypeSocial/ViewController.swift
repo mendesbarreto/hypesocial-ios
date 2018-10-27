054a7c1269cb5dfdb9d282192950b033f848a89a
Douglas Mendes <mendes-barreto@live.com>
2018-10-27 16:54:30 -0400

    Changes path from gitignore file

---
+++ HypeSocial/HypeSocial/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HypeSocial
+//
+//  Created by Douglas Mendes on 2018-10-27.
+//  Copyright © 2018 Douglas Mendes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
