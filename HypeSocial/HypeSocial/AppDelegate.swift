fec14bf052ca82e7407de876c297200fb8a55f46
Douglas Mendes <mendes-barreto@live.com>
2018-12-12 17:09:35 -0500

    Add firebase messaging to project

--- HypeSocial/HypeSocial/AppDelegate.swift
+++ HypeSocial/HypeSocial/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MSAL
+import Firebase
 
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,11 +18,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
+        FirebaseApp.configure()
         MSALLogger.shared().setCallback { (logLevel, message, containsPII) in
-
             if (!containsPII) {
-
                 print("%@", message!)
             }
         }
